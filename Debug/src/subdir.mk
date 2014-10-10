7f7d1ef141cea3d73adf9077e510c1f00bea82b7
Rajnai Viktor <rajnai.viktor@gmail.com>
2014-10-10 20:02:19 +0200

    no message

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/BlinkLed.c \
+../src/Timer.c \
+../src/_initialize_hardware.c \
+../src/main.c 
+
+OBJS += \
+./src/BlinkLed.o \
+./src/Timer.o \
+./src/_initialize_hardware.o \
+./src/main.o 
+
+C_DEPS += \
+./src/BlinkLed.d \
+./src/Timer.d \
+./src/_initialize_hardware.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_ITM -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
